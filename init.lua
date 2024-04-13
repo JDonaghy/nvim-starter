0df7094f4202be3a08fcf8579f111196ee0de849
John Donaghy <johnfdonaghy@gmail.com>
2024-04-13 17:44:40 -0500

    add missing dependancy

--- init.lua
+++ init.lua
@@ -419,6 +419,7 @@ require('lazy').setup({
   {'rcarriga/nvim-dap-ui',
     dependencies = {
       "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
     },
     config = function()
       local dap, dapui = require("dap"), require("dapui")
