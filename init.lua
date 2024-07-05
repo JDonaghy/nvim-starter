0c7c3ad02c7de94cf7b75c946509d410b7c5ba1f
John Donaghy <johnfdonaghy@gmail.com>
2024-07-05 16:29:18 -0500

    replace nvim dap woth vimspector

--- init.lua
+++ init.lua
@@ -294,11 +294,6 @@ require('lazy').setup({
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
-  {'nvim-telescope/telescope-dap.nvim',
-    config = function()
-      require('telescope').load_extension('dap')
-    end,
-  },
   {'voldikss/vim-floaterm',
     config = function()
       local set = vim.keymap.set
@@ -388,136 +383,12 @@ require('lazy').setup({
       })
     end,
     cmd = "Glow"},
-  {'mfussenegger/nvim-dap',
-    -- Based somehwat on https://aaronbos.dev/posts/debugging-csharp-neovim-nvim-dap
+  {'puremourning/vimspector',
+    cmd = { "VimspectorInstall", "VimspectorUpdate" },
+    fn = { "vimspector#Launch()", "vimspector#ToggleBreakpoint", "vimspector#Continue" },
     config = function()
-      local set = vim.keymap.set
-
-      local continue = function()
-        if vim.fn.filereadable('.vscode/launch.json') then
-          require('dap.ext.vscode').load_launchjs(nil, { coreclr = {'cs'} })
-        end
-        local dap = require('dap')
-        dap.adapters.coreclr = {
-          type = 'executable',
-          command = vim.fn.expand('$HOME/netcoredbg/netcoredbg'),
-          args = {'--interpreter=vscode'}
-        }
-        require('dap').continue()
-      end
-      set('n', '<F5>', continue)
-      set('n', '<F10>', require('dap').step_over)
-      set('n', '<F11>', require('dap').step_into)
-      set('n', '<F12>', require('dap').step_out)
-      set('n', '<Leader>b', require('dap').toggle_breakpoint)
-      set('n', '<Leader>B', require('dap').set_breakpoint)
-      set('n', '<Leader>lp>', require('dap').set_breakpoint)
-      set('n', '<Leader>dr>', require('dap').repl.open)
-      set('n', '<Leader>dl>', require('dap').run_last)
-    end,
-  },
-  {'rcarriga/nvim-dap-ui',
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "nvim-neotest/nvim-nio",
-    },
-    config = function()
-      local dap, dapui = require("dap"), require("dapui")
-
-    dapui.setup({
-      icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
-      mappings = {
-        -- Use a table to apply multiple mappings
-        expand = { "<CR>", "<2-LeftMouse>" },
-        open = "o",
-        remove = "d",
-        edit = "e",
-        repl = "r",
-        toggle = "t",
-      },
-      -- Use this to override mappings for specific elements
-      element_mappings = {
-        -- Example:
-        -- stacks = {
-        --   open = "<CR>",
-        --   expand = "o",
-        -- }
-      },
-      -- Expand lines larger than the window
-      -- Requires >= 0.7
-      expand_lines = vim.fn.has("nvim-0.7") == 1,
-      -- Layouts define sections of the screen to place windows.
-      -- The position can be "left", "right", "top" or "bottom".
-      -- The size specifies the height/width depending on position. It can be an Int
-      -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-      -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-      -- Elements are the elements shown in the layout (in order).
-      -- Layouts are opened in order so that earlier layouts take priority in window sizing.
-      layouts = {
-        {
-          elements = {
-          -- Elements can be strings or table with id and size keys.
-            { id = "scopes", size = 0.25 },
-            "breakpoints",
-            "stacks",
-            "watches",
-          },
-          size = 40, -- 40 columns
-          position = "left",
-        },
-        {
-          elements = {
-            "repl",
-            "console",
-          },
-          size = 0.25, -- 25% of total lines
-          position = "bottom",
-        },
-      },
-      controls = {
-        -- Requires Neovim nightly (or 0.8 when released)
-        enabled = true,
-        -- Display controls in this element
-        element = "repl",
-        icons = {
-          pause = "",
-          play = "",
-          step_into = "",
-          step_over = "",
-          step_out = "",
-          step_back = "",
-          run_last = "",
-          terminate = "",
-        },
-      },
-      floating = {
-        max_height = nil, -- These can be integers or a float between 0 and 1.
-        max_width = nil, -- Floats will be treated as percentage of your screen.
-        border = "single", -- Border style. Can be "single", "double" or "rounded"
-        mappings = {
-          close = { "q", "<Esc>" },
-        },
-      },
-      windows = { indent = 1 },
-      render = {
-        max_type_length = nil, -- Can be integer or nil.
-        max_value_lines = 100, -- Can be integer or nil.
-      }
-  })
-
-
-  dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open()
-  end
-  dap.listeners.before.event_terminated["dapui_config"] = function()
-    dapui.close()
-  end
-  dap.listeners.before.event_exited["dapui_config"] = function()
-    dapui.close()
-  end
-    end
-	
-  },	
+      require("vimspector").setup()
+    end,}
 }, {})
 
 -- [[ Setting options ]]
