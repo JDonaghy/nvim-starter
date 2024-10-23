0eb131cbd14f3f517afb9695cb94d43abe3772cf
John Donaghy <johnfdonaghy@gmail.com>
2024-10-23 12:24:45 -0500

    run lazygit via tmux not neovim

--- init.lua
+++ init.lua
@@ -374,25 +374,25 @@ require('lazy').setup({
       set('n', '<Leader>tf', '<Cmd>FloatermToggle!<CR>')
     end,
   },
-  {
-   'kdheepak/lazygit.nvim',
-   cmd = {
-    'LazyGit',
-    'LazyGitConfig',
-    'LazyGitCurrentFile',
-    'LazyGitFilter',
-    'LazyGitFilterCurrentFile',
-   },
-   -- optional for floating window border decoration
-   dependencies = {
-    "nvim-lua/plenary.nvim",
-   },
-   -- setting the keybinding for LazyGit with 'keys' is recommended in
-   -- order to load the plugin when the command is run for the first time
-   keys = {
-    { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
-   },
-  },
+  -- {
+  --  'kdheepak/lazygit.nvim',
+  --  cmd = {
+  --   'LazyGit',
+  --   'LazyGitConfig',
+  --   'LazyGitCurrentFile',
+  --   'LazyGitFilter',
+  --   'LazyGitFilterCurrentFile',
+  --  },
+  --  -- optional for floating window border decoration
+  --  dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  --  },
+  --  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  --  -- order to load the plugin when the command is run for the first time
+  --  keys = {
+  --   { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+  --  },
+  -- },
   {'akinsho/toggleterm.nvim', version = "*",
     config = function()
       require("toggleterm").setup{
